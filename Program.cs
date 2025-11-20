c887219620d2d39127247da4aa9dd1c051f4e3e7
auhauger@gmail.com <114182613+ahauger27@users.noreply.github.com>
2025-11-19 21:23:18 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// we'll use the Random class later to fill an array with randomly generated numbers.
+Random numberGenerator = new Random();
+
+// declare an array, 'numbers', and set each entry to 0.
+int[] numbers = [0, 0, 0, 0, 0, 0, 0];
+// store the length of 'numbers' in a variable, arrayLength.
+int arrayLength = numbers.Length;
+
+// starting at index 0, we'll iterate through the array
+for (int i = 0; i < arrayLength; i++)
+{
+    // each iteration, the array item will be assigned a random number.
+    numbers[i] = numberGenerator.Next(1, 101);
+    
+    // then check wheter each generated number is evenly divisble by 0.
+    if ((numbers[i] % 2) == 0)
+    {
+        // if true, the number is even.
+        Console.WriteLine($"The number {numbers[i]} is even.");
+    }
+    else
+    {
+        // if not, then the number is odd.
+        Console.WriteLine($"The number {numbers[i]} is odd.");
+    }
+}
