e209f96eb474537ac4b826343c7675f1c1e99f17
auhauger@gmail.com <114182613+ahauger27@users.noreply.github.com>
2025-12-15 08:09:34 -0500

    update

--- Program.cs
+++ Program.cs
@@ -1,15 +1,44 @@
-﻿int number = GetUserNumber();
+﻿Loop();
+Console.WriteLine("Goodbye");
+
+void Loop()
+{
+    while (true)
+    {
+        Console.WriteLine("Enter a number, and I'll tell you whether it's odd or even. Press 'Q' to quit.");
+        string? input = Console.ReadLine();
+
+        try
+        {
+            input = input.ToUpper();
+        }
+        catch (NullReferenceException)
+        {
+            Console.WriteLine("Please input a number")
+        }
+        if (input == "Q")
+        {
+            break;
+        }
+        else
+        {
+            int number = GetUserNumber();
+            CheckOddOrEven(number);
+            Console.WriteLine("");
+        }
+    }
+}
 
 int GetUserNumber()
 {
     Console.Write("Enter a number: ");
-    
+    int number = 0;
     try
     {
         string? userInput = Console.ReadLine();
-        int number = int.Parse(userInput);
+        number = int.Parse(userInput);
     }
-    catch (FormatException ex)
+    catch (FormatException)
     {
         Console.WriteLine($"Please input a number: ");
     }
@@ -18,4 +47,16 @@ int GetUserNumber()
         Console.WriteLine($"Please enter a number between {int.MinValue} and {int.MaxValue}:");
     }
     return number;  
+}
+
+string CheckOddOrEven(int number)
+{
+    if (number % 2 == 0)
+    {
+        return "Even"; 
+    }
+    else
+    {
+        return "Odd";
+    }
 }
