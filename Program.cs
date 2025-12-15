15020359f551ed65a64b4b1a9e663eb92581ee76
auhauger@gmail.com <114182613+ahauger27@users.noreply.github.com>
2025-12-14 20:55:22 -0500

    a different approach

--- Program.cs
+++ Program.cs
@@ -1,26 +1,21 @@
-﻿// we'll use the Random class later to fill an array with randomly generated numbers.
-Random numberGenerator = new Random();
+﻿int number = GetUserNumber();
 
-// declare an array, 'numbers', and set each entry to 0.
-int[] numbers = [0, 0, 0, 0, 0, 0, 0];
-// store the length of 'numbers' in a variable, arrayLength.
-int arrayLength = numbers.Length;
-
-// starting at index 0, we'll iterate through the array
-for (int i = 0; i < arrayLength; i++)
+int GetUserNumber()
 {
-    // each iteration, the array item will be assigned a random number.
-    numbers[i] = numberGenerator.Next(1, 101);
+    Console.Write("Enter a number: ");
     
-    // then check wheter each generated number is evenly divisble by 0.
-    if ((numbers[i] % 2) == 0)
+    try
     {
-        // if true, the number is even.
-        Console.WriteLine($"The number {numbers[i]} is even.");
+        string? userInput = Console.ReadLine();
+        int number = int.Parse(userInput);
     }
-    else
+    catch (FormatException ex)
     {
-        // if not, then the number is odd.
-        Console.WriteLine($"The number {numbers[i]} is odd.");
+        Console.WriteLine($"Please input a number: ");
     }
+    catch (OverflowException)
+    {
+        Console.WriteLine($"Please enter a number between {int.MinValue} and {int.MaxValue}:");
+    }
+    return number;  
 }
